7e55dd0d85818c0c7b888dbe1508c69143d130e5
ITstart <itstart@itstep.loc>
2022-04-20 14:17:53 +0600

    first commit

---
+++ apollo.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  client: {
+    service: {
+      name: "my-app",
+      url: "http://localhost:8055",
+    },
+    includes: [
+      "./src/**/*.js",
+      "./src/**/*.vue",
+    ],
+  }
+};
